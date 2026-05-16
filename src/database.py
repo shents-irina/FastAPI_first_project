ca44be3d62e3806e74f48285bb08153158f2b06b
Irina Sh <shents.irina@gmail.com>
2026-05-16 20:46:02 +0700

    Добавить базовый класс Base и ORM‑модель для отелей

--- src/database.py
+++ src/database.py
@@ -1,11 +1,13 @@
-import asyncio
-
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy import text
+from sqlalchemy.orm import DeclarativeBase
 
 from src.config import settings
 
 
 engine = create_async_engine(url=settings.DB_URL)
 
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
 
+
+class Base(DeclarativeBase):
+    pass
