eeb8c3c258d1c160616c4b12ecca031dedebbe99
Irina Sh <shents.irina@gmail.com>
2026-05-17 16:19:26 +0700

    Создать модель номеров

---
+++ src/models/rooms.py
@@ -0,0 +1,15 @@
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database import Base
+
+
+class RoomsORM(Base):
+    __tablename__ = "rooms"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    hotel_id: Mapped[int] = mapped_column(ForeignKey("hotels.id"))
+    title: Mapped[str]
+    description: Mapped[str | None]
+    price: Mapped[int]
+    quantity: Mapped[int]
