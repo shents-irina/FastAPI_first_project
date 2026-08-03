c461850cca529a17be3fe692f276149e0c152fbc
Irina Shentseva <shents.irina@gmail.com>
2026-08-03 15:37:44 +0700

    feat(redis): добавить подключение к Redis

---
+++ src/connectors/redis_connector.py
@@ -0,0 +1,24 @@
+import redis.asyncio as redis
+
+
+class RedisManager:
+    def __init__(self, host: str, port: int):
+        self.host = host
+        self.port = port
+        self.redis = None
+
+    async def connect(self):
+        self.redis = redis.Redis(host=self.host, port=self.port)
+
+    async def set(self, key: str, value: str, expire: int | None = None):
+        await self.redis.set(key, value, ex=expire)
+
+    async def get(self, key: str):
+        return await self.redis.get(key)
+
+    async def delete(self, key: str):
+        await self.redis.delete(key)
+
+    async def close(self):
+        if self.redis:
+            await self.redis.aclose()
---
+++ src/init.py
@@ -0,0 +1,8 @@
+from connectors.redis_connector import RedisManager
+from config import settings
+
+
+redis_manager = RedisManager(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+)
