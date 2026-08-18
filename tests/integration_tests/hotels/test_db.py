a6f5154cb0c008100075e98042bd37df49ef59c7
Irina Shentseva <shents.irina@gmail.com>
2026-08-18 14:28:07 +0700

    test: добавить регистрацию тестового пользователя и перейти на null_pool в тестах отелей

--- tests/integration_tests/hotels/test_db.py
+++ tests/integration_tests/hotels/test_db.py
@@ -1,10 +1,10 @@
-from database import async_session_maker
+from database import async_session_maker_null_pool
 from schemas.hotels import HotelAdd
 from utils.db_manager import DBManager
 
 
 async def test_add_hotel():
     hotel_data = HotelAdd(title="Отель 5 звезд", location="г. Сочи, ул. Морская, 1")
-    async with DBManager(session_factory=async_session_maker) as db:
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
         await db.hotels.add(hotel_data)
         await db.commit()
