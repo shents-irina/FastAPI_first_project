c4425179e489c0319eaeae494f23c2b56d5ae44d
Irina Shentseva <shents.irina@gmail.com>
2026-08-24 17:35:46 +0700

    test(conftest): отключить fastapi-cache в тестах

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
 import json
 from typing import AsyncGenerator
+from unittest import mock
+
+mock.patch("fastapi_cache.decorator.cache", lambda *args, **kwargs: lambda f: f).start()
 
 import pytest
 from httpx import ASGITransport, AsyncClient
